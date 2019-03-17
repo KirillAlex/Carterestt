fe8f428d4b80d0a286e9e7967519cc80aaa4a5cc
KirillAlex <pasatss20@gmail.com>
2019-03-17 11:39:16 +0200

    Initial commit

---
+++ Data/BrandPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class BrandPost
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int PostId { get; set; }
+
+        public virtual Brand Brand { get; set; }
+        public virtual Post Post { get; set; }
+
+    }
+}
---
+++ Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Data.CarContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Data.CarContext";
+        }
+
+        protected override void Seed(Data.CarContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
